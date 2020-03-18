bb8b8dc4f04d9539fef0ba564c1da173aa1533b1
vemv <vemv@users.noreply.github.com>
2020-03-18 15:20:36 +0100

    v1.1.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
-(defproject com.nedap.staffing-solutions/hier-set "1.1.2"
+(defproject com.nedap.staffing-solutions/hier-set "1.1.3"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[com.nedap.staffing-solutions/speced.def "2.0.0"]
                  [org.clojure/clojure "1.10.1"]]
